16fba55f36042d6ac186d06a3633ea6889a8fdd9
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-02-16 01:52:08 +0200

    Fix authMiddleware and add route guards authorization

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,9 +1,6 @@
 import jsonwebtoken from 'jsonwebtoken';
 
-import {
-    AUTH_COOKIE_NAME,
-    JWT_SECRET
-} from "../config.js"
+import { AUTH_COOKIE_NAME, JWT_SECRET } from "../config.js"
 
 export const auth = (req, res, next) => {
     const token = req.cookies[AUTH_COOKIE_NAME];
@@ -17,10 +14,19 @@ export const auth = (req, res, next) => {
 
         req.user = decodedToken; // Attach user to request
         res.locals.user = decodedToken; // Attach user to handlebars context
-        
-        next();
+
     } catch (err) {
         res.clearCookie(AUTH_COOKIE_NAME);
-        res.redirect('/auth/login');
+        return res.redirect('/auth/login');
     }
+
+    next();
+};
+
+export const isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/auth/login');
+    }
+
+    next();
 }
